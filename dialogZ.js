6fe255568364e382ddf8bca87e6ea637b80c8635
张鑫14 <zhangxin14@hikvision.com.cn>
2017-03-24 11:45:11 +0800

    增加dialog显示按钮选择

--- dialogZ.js
+++ dialogZ.js
@@ -1,10 +1,9 @@
-(function ($) {
+(function($) {
     //构造函数
-    var DialogZ = function (element, options) {
+    var DialogZ = function(element, options) {
         //这边两个参数是从 new DialogZ(this,option) 中传过来的
         this.$element = $(element);
         this.options = options;
-        this.$ctrlsClose = this.$element.find('[data-dialog-close]');
     };
     // 默认参数
     DialogZ.defaults = {
@@ -13,49 +12,57 @@
         nDefaultTimeCloseDialog: 0,
         isOpen: false,
         inProgress: false,
-        // support: {
-        //     animations: Modernizr.cssanimations
-        // },
-        // animEndEventNames: {
-        //     'WebkitAnimation': 'webkitAnimationEnd',
-        //     'OAnimation': 'oAnimationEnd',
-        //     'msAnimation': 'MSAnimationEnd',
-        //     'animation': 'animationend'
-        // },
-        onFirstOpenDialog: function () {
+        content: null,
+        showButton: false,
+        sureBtnText: '确定',
+        cancelBtnText: '取消',
+        onFirstOpenDialog: function() {
             return false;
         },
-        onBeforeOpenDialog: function () {
+        onBeforeOpenDialog: function() {
             return false;
         },
-        onOpenDialog: function () {
+        onOpenDialog: function() {
             return false;
         },
-        onBeforeCloseDialog: function () {
+        onBeforeCloseDialog: function() {
             return false;
         },
-        onCloseDialog: function () {
+        onCloseDialog: function() {
             return false;
         },
-
+        onSureClick: function() {
+            return false;
+        }
     };
 
 
-    DialogZ.prototype.render = function(){
-        var self = this;
-        var _wrap = '<div class="dialog__overlay"></div><div class="dialog__content"></div>';
-        var _content = this.$element.children().clone();
-        
-        this.$element.html(_wrap);
-        this.$element.find('.dialog__content').append(_content);
-                
-        // 给多个关闭的功能的元素绑定close事件
-        this.$ctrlsClose.each(function () {
-            $(this).on('click', this.close.bind(this));
-        });
+    DialogZ.prototype.render = function() {
+        var self = this,
+            _content,
+            bottonGroup;
+        if (this.options.content) {
+            var _wrap = '<div class="dialog__overlay"></div><div class="dialog__content"></div>';
+            this.$element.html(_wrap);
+            this.$element.find('.dialog__content').append(this.options.content);
+        } else {
+            this.$element.find('.dialog__content').append(this.$element.content);
+        }
+        if (this.options.showButton) {
+            bottonGroup = '<div class="dialog__button">' +
+                '<button class="dialog__btn dialog__sure">' + this.options.sureBtnText + '</button>' +
+                '<button class="dialog__btn dialog__cancel">' + this.options.cancelBtnText + '</button>' +
+                '</div>'
+            this.$element.find('.dialog__content').append(bottonGroup);
+            // 给多个关闭的功能的元素绑定close事件
+            this.$element.find('.dialog__cancel').on('click', this.close.bind(this));
+
+            this.$element.find('.dialog__sure').on('click', this.options.onSureClick);
+        }
+
 
         // esc键关闭dialog
-        $(document).on('keydown', function (ev) {
+        $(document).on('keydown', function(ev) {
             var keyCode = ev.keyCode || ev.which;
             if (keyCode === 27 && self.options.isOpen) {
                 self.close();
@@ -67,12 +74,12 @@
             $oDialogOVerlay.on('click', this.close.bind(this));
         }
     };
-    
+
 
     /**
      * 打开dialog
      */
-    DialogZ.prototype.show = function () {
+    DialogZ.prototype.show = function() {
         var _that = this;
         //正在操作dialog（关&闭）
         this.options.inProgress = true;
@@ -82,7 +89,7 @@
             this.options.onFirstOpenDialog(this);
             this.options.isEverOpen = true;
         }
-        setTimeout(function () {
+        setTimeout(function() {
             _that.$element.addClass('dialog--open');
 
             _that.options.onOpenDialog(_that);
@@ -96,14 +103,14 @@
     /**
      * 关闭dialog
      */
-    DialogZ.prototype.close = function () {
+    DialogZ.prototype.close = function() {
         this.options.onBeforeOpenDialog(this);
 
         var _that = this;
 
         this.options.onBeforeCloseDialog(this);
 
-        setTimeout(function () {
+        setTimeout(function() {
             _that.$element.removeClass('dialog--open');
 
             _that.options.onCloseDialog(_that);
@@ -115,7 +122,7 @@
     };
 
     function Plugin(option, params) {
-        return this.each(function () {
+        return this.each(function() {
             var $this = $(this),
                 data = $this.data('bs.DialogZ'),
                 options = $.extend({}, DialogZ.defaults, option);
@@ -135,7 +142,7 @@
     //重新将`$.fn.DialogZ`的`Constructor`指向为插件的构造函数`DialogZ`,因为`Constructor`可以被认为的修改掉
     $.fn.dialogZ.Constructor = DialogZ
     //防止命名冲突
-    $.fn.dialogZ.noConflict = function () {
+    $.fn.dialogZ.noConflict = function() {
         $.fn.dialogZ = old
         return this
     }
